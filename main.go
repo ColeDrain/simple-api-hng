7d137461281906e8d406e815f317c58685dbf3c4
Ugochukwu Onyebuchi <ugochukwu.onyebuchi@indicina.co>
2022-11-05 10:18:58 +0100

    feat: added calculate endpoint

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	gogpt "github.com/sashabaranov/go-gpt3"
 )
 
 // user represents data about user.
@@ -15,7 +20,19 @@ type user struct {
     Bio  string `json:"bio"`
 }
 
-// albums slice to seed record album data.
+type RequestOperation struct {
+	Op string `json:"operation_type,omitempty"`
+	X             int    `json:"x,omitempty"`
+	Y             int    `json:"y,omitempty"`
+}
+
+type ResponseOperation struct {
+	SlackUsername string `json:"slackUsername,omitempty"`
+	Result        int    `json:"result,omitempty"`
+	OperationType string `json:"operation_type,omitempty"`
+	Error         string `json:"error,omitempty"`
+}
+
 var users = []user{
     { SlackUsername: "anonymous", Backend: true, Age: 23, Bio: "I am everywhere, more like the Davinci of Tech."},
 }
@@ -24,6 +41,7 @@ func main() {
     router := gin.Default()
 
     router.GET("/anon", getme)
+    router.POST("/calculate", calculate)
 
 	port := os.Getenv("PORT")
     router.Run("0.0.0.0:"+port)
@@ -36,4 +54,72 @@ func getme(c *gin.Context) {
     c.Header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS")
 
     c.IndentedJSON(http.StatusOK, users[0])
+}
+
+
+func calculate(c *gin.Context) {
+    var newOp RequestOperation
+
+    resp := new(ResponseOperation)
+    resp.SlackUsername = "SonOfVinci"
+
+    // Call BindJSON to bind the received JSON to newOperation
+    if err := c.BindJSON(&newOp); err != nil {
+        c.IndentedJSON(http.StatusBadRequest, err)
+    }
+
+    switch newOp.Op {
+    case "addition":
+        resp.Result = newOp.X + newOp.Y
+    case "subtraction":
+        resp.Result = newOp.X - newOp.Y
+    case "multiplication":
+        resp.Result = newOp.X * newOp.Y
+    default:
+        fmt.Println(gptCalculate(newOp.Op))
+
+        res, err := gptCalculate(newOp.Op)
+        if err != nil {
+            resp.Error = "Bad Input 🤔"
+        } else {
+            resp.Result = res
+        }
+    }
+
+    resp.OperationType = newOp.Op
+
+    c.IndentedJSON(http.StatusOK, resp)
+}
+
+func gptCalculate(prompt string) (int, error){
+    c := gogpt.NewClient("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")
+	ctx := context.Background()
+
+	req := gogpt.CompletionRequest{
+		Model: "text-davinci-002",
+        Prompt: prompt,
+        Temperature: 0.3,
+        MaxTokens: 60,
+        TopP: 1,
+        FrequencyPenalty: 0.8,
+        PresencePenalty: 0,
+	}
+
+    resp, err := c.CreateCompletion(ctx, req)
+	if err != nil {
+		return -1, err
+	}
+
+    // Result from OpenAI
+    result := resp.Choices[0].Text
+
+    // Get only the number if possible
+    s := strings.Split(result, "=")
+    slast := s[len(s)-1]
+    strim := strings.TrimSpace(slast)
+    i, err := strconv.Atoi(strim)
+    if err != nil {
+        return -1, err
+    }
+    return i, nil
 }
